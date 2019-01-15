863ce5a16d5f3cbbcdc8f33cde2e09873d3d1d1b
Frodo061 <joao_pedro.1997@hotmail.com>
2019-01-15 16:36:22 +0000

    Setup.py to install dependencies.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
 setup(name='tstp3',
-      version='1.0',
-      description='Authenticated secure fuse file system',
-      packages=['pyfuse3'],
-      zip_safe=False)
+    version='1.0',
+    description='Authenticated secure fuse file system',
+    packages=['pyfuse3'],
+    install_requires=[
+        'pyfuse3',
+        'cryptography',
+        'python-pam'
+    ],
+    zip_safe=False)
