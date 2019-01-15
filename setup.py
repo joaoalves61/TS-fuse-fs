6adddace10e49d36432fff56f917063498a26f8c
Frodo061 <joao_pedro.1997@hotmail.com>
2019-01-15 16:19:26 +0000

    Setup.py added.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='tstp3',
+      version='1.0',
+      description='Authenticated secure fuse file system',
+      packages=['pyfuse3'],
+      zip_safe=False)
